f9922a092a04210a58a73ec4a41ab06de1473f54
mekladious <miraekladious@gmail.com>
2017-04-25 02:43:35 +0200

    fixed error

--- decode.v
+++ decode.v
@@ -2,8 +2,8 @@ module decode(instr, op, rs, rt, rd, immed);
 
 input [31:0] instr;
 output wire [5:0] op;
-wire [4:0] rs, rt, rd;
-wire [15:0] immed;
+output wire [4:0] rs, rt, rd;
+output wire [15:0] immed;
 
 assign op = instr[31:26];
 assign rs = instr[25:21];
