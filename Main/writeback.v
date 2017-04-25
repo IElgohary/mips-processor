e7c31175133e22f9c77349bcc83e7e1864dbb3c4
mekladious <miraekladious@gmail.com>
2017-04-25 18:10:44 +0200

    finished, not tested

--- Main/writeback.v
+++ Main/writeback.v
@@ -11,7 +11,7 @@ output wire [4:0] rd_out;
 output wire regWrite_out;
 output wire [31:0] writeData;
 
-MEM-WB wb(ALUres_out, readData_out, rd_out, regWrite_out, memToReg_out, ALUres, readData, rd, regWrite, memToReg, clk);
+MEMWBreg wb(ALUres_out, readData_out, rd_out, regWrite_out, memToReg_out, ALUres, readData, rd, regWrite, memToReg, clk);
 
 mux m (writeData, readData_out , ALUres_out, memToReg_out);
 
