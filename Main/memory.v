7247e47a39c250aecad05e7462f4442a89ccfc0b
mekladious <miraekladious@gmail.com>
2017-04-25 18:47:02 +0200

    included ex-mem reg

--- Main/memory.v
+++ Main/memory.v
@@ -1,9 +1,7 @@
-module memory(ALUres_out, readData, rd_out, WB_out, branch, branchAddr_out, MEM, WB, branchAddr, zero, ALUres, writeData, rd, clk);
+module memory(ALUres_out, readData, rd_out, WB_out, branch, branchAddr_out, branch_in, memWrite, memRead, regWrite, memToReg, branchAddr, zero, ALUres, writeData, rd, clk);
 
-input [2:0] MEM;
-input [1:0] WB;
+input branch_in, memWrite, memRead, regWrite, memToReg, zero, clk;
 input [31:0] branchAddr, ALUres, writeData;
-input zero, clk;
 input [4:0] rd;
 
 output wire [31:0] ALUres_out, readData, branchAddr_out;
@@ -11,12 +9,14 @@ output wire [4:0] rd_out;
 output wire [1:0] WB_out;
 output wire branch;
 
-data_memory dm(readData, ALUres, writeData, MEM[1], MEM[0], clk);
+wire branch_out, memWrite_out, memRead_out, PCSrc_out, memToReg_out, zero_out, regWrite_out;
+wire [31:0] writeData_out;
 
-assign branch = zero & MEM[2];
-assign ALUres_out = ALUres;
-assign rd_out = rd;
-assign WB_out = WB;
-assign branchAddr_out = branchAddr;
+EXMEMreg mem(branchAddr_out, zero_out, ALUres_out, writeData_out, rd_out, branch_out, memRead_out, memWrite_out, regWrite_out,memToReg_out,
+                branchAddr, zero, ALUres, writeData, rd, branch_in, memRead, memWrite, regWrite, memToReg, clk);
+
+data_memory dm(readData, ALUres_out, writeData_out, memRead_out, memWrite_out, clk);
+
+assign branch = zero & branch_out;
 
 endmodule
